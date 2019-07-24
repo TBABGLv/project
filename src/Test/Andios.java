446a88254a9ec22994fe79b23dc0bedf297549cc
zhaoqiongni <2447145663@qq.com>
2019-07-24 20:23:30 +0800

    没什么

---
+++ src/Test/Andios.java
@@ -0,0 +1,30 @@
+package Test;
+
+public class Andios {
+    private Person teaXv;
+    private Person stu;
+
+    public Andios(Person teaXv, Person stu) {
+        this.teaXv = teaXv;
+        this.stu = stu;
+    }
+
+    public Andios() {
+    }
+
+    public Person getTeaXv() {
+        return teaXv;
+    }
+
+    public void setTeaXv(Person teaXv) {
+        this.teaXv = teaXv;
+    }
+
+    public Person getStu() {
+        return stu;
+    }
+
+    public void setStu(Person stu) {
+        this.stu = stu;
+    }
+}
---
+++ src/Test/Person.java
@@ -0,0 +1,40 @@
+package Test;
+
+public class Person {
+    private String name;
+    private Integer age;
+    private Integer sex;
+
+    public Person(String name, Integer age, Integer sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+}
